510785e7a1fff63d6de47120d8ee20018cce163e
CZ010 <starj1918@gmail.com>
2021-05-13 22:52:25 +0600

    create notifications hook

---
+++ src/Hooks/Notifications/Notifications.js
@@ -0,0 +1,29 @@
+import {useSnackbar} from "notistack";
+import {Slide} from "@material-ui/core";
+
+export const useNotifications = () => {
+  const {enqueueSnackbar} = useSnackbar();
+  const Success = (message) => {
+    enqueueSnackbar(message, {
+      variant: "success",
+      anchorOrigin: {
+        vertical: "top",
+        horizontal: "right",
+      },
+      TransitionComponent: Slide,
+    });
+  };
+
+  const Error = (message) => {
+    enqueueSnackbar(message, {
+      variant: "error",
+      anchorOrigin: {
+        vertical: "top",
+        horizontal: "right",
+      },
+      TransitionComponent: Slide,
+    });
+  };
+
+  return {Success, Error};
+};
--- src/index.js
+++ src/index.js
@@ -2,12 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import {StylesProvider} from "@material-ui/core";
+import {IconButton, StylesProvider} from "@material-ui/core";
+import {SnackbarProvider} from "notistack";
+import HighlightOffIcon from "@material-ui/icons/HighlightOff";
+
+const notistackRef = React.createRef();
+const onClickDismiss = key => () => {
+  notistackRef.current.closeSnackbar(key);
+};
 
 ReactDOM.render(
   <StylesProvider injectFirst>
-    <App/>
+    <SnackbarProvider
+      ref={notistackRef}
+      action={(key) => (
+        <IconButton style={{color: "#fff"}} onClick={onClickDismiss(key)}>
+          <HighlightOffIcon/>
+        </IconButton>
+      )}
+    >
+      <App/>
+    </SnackbarProvider>
   </StylesProvider>,
   document.getElementById("root")
 );
+
 
