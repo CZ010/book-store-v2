1b672538b58ad0c7e397cfcfe2f06007d4a49f66
CZ010 <starj1918@gmail.com>
2021-05-13 22:50:54 +0600

    Update CartButton.jsx

--- src/UI-kit/Buttons/CartButton/CartButton.jsx
+++ src/UI-kit/Buttons/CartButton/CartButton.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import {Badge, IconButton} from "@material-ui/core";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 
-export const CartButton = ({onClick, badgeContent}) => {
+export const CartButton = ({onClick, badgeContent, ...props}) => {
   return (
     <>
       <IconButton
         onClick={onClick}
         color="inherit"
         className="user-icon-btn"
+        {...props}
       >
         <Badge badgeContent={badgeContent} color="secondary">
           <ShoppingCartIcon/>
