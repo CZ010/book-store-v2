3d9d2630430a52880d121c98509292bbae6c0a0a
CZ010 <starj1918@gmail.com>
2021-05-17 15:14:38 +0600

    Update DataContext.js

--- src/Context/DataContext.js
+++ src/Context/DataContext.js
@@ -1,11 +1,14 @@
-import React, {createContext} from "react";
+import React, {createContext, useEffect, useState} from "react";
 import {useCookies} from "react-cookie";
+import * as Firestore from "../Services/Firestore/Firestore";
 
 export const DataContext = createContext({});
 
 export const DataProvider = ({children}) => {
   const [cookies, setCookie, removeCookie] = useCookies(["user"]);
   const user = cookies.user;
+
+  const [cart, setCart] = useState({books: []});
 
   const login = userData => {
     setCookie("user", userData, {path: "/", maxAge: 2592000});
@@ -15,13 +18,44 @@ export const DataProvider = ({children}) => {
     removeCookie("user");
   };
 
+  useEffect(() => {
+    if (user) {
+      Firestore.getUserCart(user.id).then(cart => {
+        setCart(cart);
+      });
+    } else {
+      setCart({books: []});
+    }
+  }, [user]);
+
+  const addToCart = (bookID) => {
+    Firestore.updateUserCart(user.id, bookID).catch(error => {
+      console.error(error);
+    });
+    Firestore.getUserCart(user.id).then(cart => {
+      setCart(cart);
+    });
+  };
+
+  const removeFromCart = (bookID) => {
+    Firestore.removeBookFromUserCart(user.id, bookID).catch(error => {
+      console.error(error);
+    });
+    Firestore.getUserCart(user.id).then(cart => {
+      setCart(cart);
+    });
+  };
+
 
   return (
     <DataContext.Provider
       value={{
         AuthedUser: [user],
         Login: [login],
-        Logout: [logout]
+        Logout: [logout],
+        ShoppingCart: [cart],
+        AddToCart: [addToCart],
+        RemoveFromCart: [removeFromCart]
       }}
     >
       {children}
