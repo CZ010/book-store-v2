e6d1cf7638a162df17fbe8bf4fa1a7b82350c36c
CZ010 <starj1918@gmail.com>
2021-05-31 10:46:35 +0600

    Update NoMatch.jsx

--- src/Components/NoMatch/NoMatch.jsx
+++ src/Components/NoMatch/NoMatch.jsx
@@ -1,12 +1,34 @@
 import React from "react";
-import {Typography} from "@material-ui/core";
+import {Divider, Grid, makeStyles, Typography} from "@material-ui/core";
 
 const NoMatch = () => {
+  const styles = useStyles();
   return (
-    <>
-      <Typography variant={"h1"}>404</Typography>
-    </>
+    <Grid container alignItems="center" justify="center" className={styles.root}>
+      <div className={styles.container}>
+        <Typography variant={"h2"}>404</Typography>
+        <Divider orientation="vertical" flexItem/>
+        <Typography variant={"h4"}>Страница не найдена</Typography>
+      </div>
+    </Grid>
   );
 };
+
+const useStyles = makeStyles(() => ({
+  root: {
+    width: "100%",
+    height: "calc(100vh - 150px)"
+  },
+  container: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: "-10px",
+    "&>*": {
+      margin: "10px",
+      color: "rgba(0, 0, 0, 0.38)"
+    }
+  }
+}));
 
 export default NoMatch;
